2c0e55d9f9a6cc23d7f3e6319bd23db8607ed644
ykemals <y.kemalsert@gmail.com>
2020-02-07 20:08:23 +0300

    useMongoClient

--- helper/db.js
+++ helper/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
     //mongoose.connect('mongodb://newMovieSchema:nms123@ds133251.mlab.com:33251/heroku_3k5qpn72', {useMongoClient: true});
-    mongoose.connect('mongodb://localhost/moviesimdb',{useMongoClient:true});
+    mongoose.connect('mongodb://localhost/moviesimdb',{useNewUrlParser:true});
     mongoose.connection.on('open', () => {
         // console.log('MongoDB: Connected');
     });
