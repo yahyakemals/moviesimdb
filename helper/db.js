d92b215c655a0b4eae2ecb8aff21f665e2145c10
ykemals <y.kemalsert@gmail.com>
2020-02-07 18:05:44 +0300

    movis schema is done

---
+++ helper/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    //mongoose.connect('mongodb://newMovieSchema:nms123@ds133251.mlab.com:33251/heroku_3k5qpn72', {useMongoClient: true});
+    mongoose.connect('mongodb://localhost/moviesimdb',{useMongoClient:true});
+    mongoose.connection.on('open', () => {
+        // console.log('MongoDB: Connected');
+    });
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB: Error', err);
+    });
+
+    mongoose.Promise = global.Promise;
+};
